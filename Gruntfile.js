b74ff8c4dced724c313222e4f8c5a892ba1f6f61
JLee <jlee@brainbytescreative.com>
2017-09-14 14:53:50 -0400

    Index.html, built home page.
    TODO : ... everything outside the basic HTML and CSS... lol.

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-responsive-images');
 
     grunt.registerTask('default', ['concat', 'uglify']);
